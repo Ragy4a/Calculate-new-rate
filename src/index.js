510bcee447c98b3771573615da21ca8a8fc27d45
Ragy4a <122722327+Ragy4a@users.noreply.github.com>
2024-02-05 18:38:56 +0200

    Production Version #1

--- src/index.js
+++ src/index.js
@@ -3,47 +3,30 @@
 
 /**
  * 
- * @param {number} winnerRating 
- * @param {number} loserRating 
- * @returns 
+ * @param {number} winnerRating - Рейтинг Победившего
+ * @param {number} loserRating - Рейтинг проигравшего
+ * @returns - возвращает ответы при неправильном вводе и также возвращает информцаию которую выдала функция.
  */
 function calculateRating (winnerRating, loserRating) {
+    let rateDifference = winnerRating - loserRating;
     if (typeof winnerRating !== 'number' || typeof loserRating !== 'number') {
-        console.log('Unknown information')
-        return;
-    }
+        return 'Unknown information.'
+    };
     if (winnerRating < 0 || loserRating < 0){
-        console.log('Rating can not be less than 0')
-        return;
-    }
-    console.log('===================================================')
-    console.log(`Rating first player: ${winnerRating} and second player: ${loserRating}`)
-    console.log('===================================================')
-    let ratingDifference = winnerRating - loserRating;
-    if (ratingDifference >= 0 && ratingDifference <= 2) {
-        winnerRating += 2
-        console.log(`You have won. Now your rating has achieved +2. Rating ${winnerRating}`)
-    }else if (ratingDifference >= 2 && ratingDifference <= 20) {
-        winnerRating += 1
-        console.log(`You have won. Now your rating has achieved +1. Rating ${winnerRating}`)
-    }
+        return 'Rating can not be less than 0';
+    };
     if (winnerRating === 0) {
         winnerRating += loserRating;
-        console.log(`You have achieved rating: ${loserRating}`)
-    }
-    if (ratingDifference >= 20) {
-        console.log('You have won, but your rating still the same.')
-    }
-    if (loserRating - winnerRating >= 10) {
-        let formule = (loserRating - winnerRating + 5) / 3
-        winnerRating += formule
-        console.log(`You have won a strong enemie! You have achieved +${formule.toFixed(0)}`)
-    }else if (loserRating - winnerRating >= 9) {
-        winnerRating += 5
-        console.log(`You have won an enemie. You have achieved +5.`)
-    }
-    console.log('===================================================')
-    console.log(`Winner rating: ${winnerRating.toFixed(0)} and loser rating: ${loserRating}`);
+    }else if (rateDifference >= 0 && rateDifference <= 2) {
+        winnerRating += 2;
+    }else if (rateDifference > 2 && rateDifference < 20){
+        winnerRating += 1;
+    }else if (rateDifference >= 20) {
+        winnerRating += 0;
+    }else if (winnerRating < loserRating) {
+        winnerRating += (loserRating - winnerRating + 5) / 3;
+    };
+    return `Winner rating: ${winnerRating.toFixed(1)}`;
 };
 
-calculateRating(2, 35)
+console.log(calculateRating(41, 40))
